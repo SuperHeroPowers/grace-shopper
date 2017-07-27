7670bb75d5153c3de25eba01431a8c8710cc447a
Monica Choe <monicchoe@gmail.com>
2017-07-26 20:06:22 -0400

    product routes complete

--- server/api/products.js
+++ server/api/products.js
@@ -7,3 +7,31 @@ router.get('/', (req, res, next)=>{
 	.then(products => res.json(products))
 	.catch(next);
 });
+
+router.get('/:productId', (req, res, next)=>{
+	Product.findById(req.params.productId)
+	.then(product => res.json(product))
+	.catch(next);
+});
+
+router.post('/', (req, res, next)=>{
+	Product.create(req.body)
+	.then(product => res.status(201).json(product))
+	.catch(next);
+});
+
+router.put('/:productId', (req, res, next)=>{
+	Product.update(req.body, {where: {id : campusId}, returning: true})
+	.then(product => res.status(200).json(product))
+	.catch(next);
+});
+
+router.delete('/:productId', (req, res, next)=> {
+	Product.destory({
+		where : {
+			id : req.params.productId
+		}
+	})
+	.then(()=>res.sendStatus(204))
+	.catch(next);
+});
