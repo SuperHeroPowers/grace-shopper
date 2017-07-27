b329a5cb796f16955314cff368d866df27bea459
Monica Choe <monicchoe@gmail.com>
2017-07-26 20:27:09 -0400

    working on category routes

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,9 +1,30 @@
-const router = require('express').Router();
-const {Category} = require('../db/models');
-module.exports = router;
+const router = require('express').Router()
+const {Category} = require('../db/models')
 
-router.get('/', (req, res, next)=>{
-	Category.findAll()
-	.then(categories => res.json(categories))
+router.get('/', (req, res, next) => {
+  Category.findAll()
+    .then(categories => res.json(categories))
+    .catch(next);
+});
+
+router.get('/:categoryId/products', (req, res, next)=>{
+
+});
+
+router.post('/', (req, res, next)=>{
+	Category.create(req.body)
+	.then(category => res.status(201).json(category))
 	.catch(next);
 });
+
+router.delete('/:categoryId', (req, res, next)=> {
+	Category.destory({
+		where : {
+			id : req.params.categoryId
+		}
+	})
+	.then(()=>res.sendStatus(204))
+	.catch(next);
+});
+
+module.exports = router;
