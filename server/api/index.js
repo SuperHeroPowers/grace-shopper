9694f60e32885acee11f5415d222eb383b46a993
Monica Choe <monicchoe@gmail.com>
2017-07-28 15:23:41 -0400

    resolved lasting merge conflicts

--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,6 @@ module.exports = router
 
 router.use('/users', require('./users'));
 router.use('/orders', require('./orders'));
-<<<<<<< HEAD
-router.use('/users', require('./users'))
-router.use('/reviews', require('./reviews'))
-=======
->>>>>>> 7fb88c12a9014d3bb97b4fde0170dfdb91a41def
 router.use('/products', require('./products'));
 router.use('/categories', require('./categories'));
 
