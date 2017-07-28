a1bb0c0c7bdef65b6fe0fe2e3e16c78a15eb51ff
Danni Liu <dannielleorlando@gmail.com>
2017-07-28 14:38:29 -0400

    deleted seed file

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function Navbar(){
+const Navbar = (props) => {
+
+  const {handleClick, isLoggedIn} = props;
+
   return (
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
-          <Link to="/products" className="navbar-brand">Super Hero PowerUp!</Link>
+          <Link to="/" className="navbar-brand">SuperHero++</Link>
         </div>
         <div className="collapse navbar-collapse">
           <ul className="nav navbar-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/">All</Link></li>
+            <li className='active'><Link to="/">Home</Link></li>
+            <li><Link to="#">Physical</Link></li>
+            <li><Link to="#">Mental</Link></li>
+            <li><Link to="#">Manipulation</Link></li>
+            <li><Link to="#">Transportation</Link></li>
           </ul>
+          <form className="navbar-form navbar-left">
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Search"></input>
+            </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+          {
+          isLoggedIn ?
           <ul className="nav navbar-nav navbar-right">
             <li><Link to="#">My Account</Link></li>
-            <li><Link to="#">Cart</Link></li>
+            <li><Link to="#" onClick={handleClick}>Log out</Link></li>
+          </ul>
+          :
+              (<li><Link to="#">Sign up</Link></li>
+              <li><Link to="#">Log in</Link></li>)
+            </div>
+          }
+          <li><Link to="#">Cart</Link></li>
           </ul>
         </div>
       </div>
     </nav>
   );
 }
+
+export default Navbar;
