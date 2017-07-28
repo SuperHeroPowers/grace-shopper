e42f01126b7513fc5548a5ef32c889e7478b4cdf
Monica Choe <monicchoe@gmail.com>
2017-07-27 20:28:59 -0400

    set-up and complete orders redux store

--- client/store/index.js
+++ client/store/index.js
@@ -1,11 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import user from './user'
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import user from './user';
+import order from './order';
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, order})
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 const store = createStore(reducer, middleware);
 
-export default store
-export * from './user'
+export default store;
+export * from './user';
+export * from './order';
---
+++ client/store/order.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import history from '../history';
+
+// Initial State
+const orders = [];
+
+// Action Types
+const GET_ORDER = 'GET_ORDER';
+const GET_ORDERS = 'GET_ORDERS';
+const CREATE_ORDER = 'CREATE_ORDER';
+const UPDATE_ORDER = 'UPDATE_ORDER';
+
+// Action creators
+const getOrder = order => ({type: GET_ORDER, order});
+const getOrders = orders => ({type: GET_ORDERS, orders});
+const createOrder = order => ({type: CREATE_ORDER, order});
+const updateOrder = order => ({type: UPDATE_ORDER, order});
+
+// Thunk creators
+export const fetchOrders = () =>
+  	dispatch =>
+    	axios.get('/api/orders')
+      	.then(res => dispatch(getOrders(res.data || orders)))
+      	.catch(err => console.log(err));
+
+export const fetchOrder = order =>
+  	dispatch =>
+    	axios.get(`/api/orders/${order.id}`)
+      	.then(res => dispatch(getOrder(res.data || orders)))
+      	.catch(err => console.log(err));
+
+export const postOrder = order => 
+	dispatch =>
+		axios.post('/api/orders', order)
+		.then(res => dispatch(createOrder(res.data || orders)))
+		.catch(err => console.log(err));
+
+export const putOrder = order =>
+	dispatch =>
+		axios.put(`/api/orders/${order.id}`, order)
+		.then(res => dispatch(updateOrder(res.data || orders)))
+		.catch(err => console.log(err));
+
+// Reducer
+export default function (state = orders, action) {
+  switch (action.type) {
+    case GET_ORDER:
+      	return [...state, action.order];
+  	case GET_ORDERS:
+  		return action.orders;
+    case UPDATE_ORDER:
+    	return [...state, action.order];
+   	case CREATE_ORDER:
+   		return [...state, action.order];
+    default:
+      return state;
+  };
+};
