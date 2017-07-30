324f32077dcd5bc1e4c5ea842e38e4753574d0fe
Alice Chuang <bomberetta@gmail.com>
2017-07-29 22:26:30 -0400

    navbar pulling in categories

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
+import {BrowserRouter, withRouter, Route, Switch, Link} from 'react-router-dom'
 import {logout} from '../store'
 import Navbar from './Navbar'
 import Products from './Products'
@@ -13,13 +13,29 @@ import Products from './Products'
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-
   const {children, handleClick, isLoggedIn} = props;
 
   return (
     <div>
       <Navbar isLoggedIn = {isLoggedIn} handleClick={handleClick}/>
       <Products />
+      {/*<BrowserRouter>*/}
+        {/*<Switch>*/}
+          {/*/!*<Route exact path="/" component={Home} />*!/*/}
+          {/*/!*<Route exact path="/students" component={Student} />*!/*/}
+          {/*/!*<Route exact path="/campuses" component={Campus} />*!/*/}
+          {/*/!*<Route exact path="/students/new" component={NewStudent} />*!/*/}
+          {/*/!*<Route exact path="/campuses/new" component={NewCampus} />*!/*/}
+          {/*/!*<Route exact path="/students/edit/:studentId" component={NewStudent} />*!/*/}
+          {/*/!*<Route exact path="/campuses/edit/:campusId" component={NewCampus} />*!/*/}
+          {/*/!*<Route exact path="/students/view/:studentId" component={SingleStudent} />*!/*/}
+          {/*/!*<Route exact path="/campuses/view/:campusId" component={SingleCampus} />*!/*/}
+          {/*/!*<Route path="/readme" component={ReadMe} />*!/*/}
+          {/*/!*<Route path="/video" component={Video} />*!/*/}
+          {/*/!*<Route component={Products} />*!/*/}
+          {/*/!*<Route component={Root} />*!/*/}
+        {/*</Switch>*/}
+      {/*</BrowserRouter>*/}
     </div>
   )
 }
